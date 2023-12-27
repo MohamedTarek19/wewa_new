c390e1abea5b94c0391a59598d108c74c6440af4
Mohamed Tarek <mohamedtarek1428@gmail.com>
2023-12-27 16:17:30 +0200

    final for now

--- lib/presentation/views/vendor_screens/vendor_screen.dart
+++ lib/presentation/views/vendor_screens/vendor_screen.dart
@@ -4,8 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-
-
 class VendorScreen extends StatefulWidget {
   VendorScreen({super.key});
 
@@ -15,6 +13,7 @@ class VendorScreen extends StatefulWidget {
 
 class _VendorScreenState extends State<VendorScreen> {
   final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
   GoogleMapController? newGoogleMapController;
   static const CameraPosition _kGooglePlex = CameraPosition(
     target: LatLng(37.42796133580664, -122.085749655962),
@@ -27,17 +26,16 @@ class _VendorScreenState extends State<VendorScreen> {
     tilt: 59.440717697143555,
     zoom: 19.151926040649414,
   );
+
   //late Location _locationController;
 
   bool resetLocation = false;
   var markedPosition;
   bool serviceEnabled = false;
-  late LocationData currentPosition;
+  LocationData? currentPosition;
   Location locationController = Location();
 
   Locale loc = const Locale("ar");
-
-
 
   _locatePosition() async {
     setState(() {
@@ -48,16 +46,57 @@ class _VendorScreenState extends State<VendorScreen> {
     setState(() {
       currentPosition = p;
     });
-    LatLng latlngposition = LatLng(currentPosition.latitude??0, currentPosition.longitude??0);
-    CameraPosition cam = new CameraPosition(target: latlngposition, zoom: 15);
-    newGoogleMapController!.animateCamera(CameraUpdate.newCameraPosition(cam));
+    LatLng latlngposition = LatLng(currentPosition?.latitude ?? 0, currentPosition?.longitude ?? 0);
+    CameraPosition cam = CameraPosition(target: latlngposition, zoom: 15);
+    newGoogleMapController?.animateCamera(CameraUpdate.newCameraPosition(cam));
+  }
+  getLocationUpdates()async{
+    PermissionStatus _permissionGranted;
+    serviceEnabled = await locationController.serviceEnabled();
+    if (!serviceEnabled) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        duration: Duration(seconds: 5),
+        backgroundColor: Colors.red,
+        content: Text('Location services are disabled.'),
+      ));
+      serviceEnabled = await locationController.requestService();
+    }
+    _permissionGranted = await locationController.hasPermission();
+    if (_permissionGranted == PermissionStatus.denied) {
+      _permissionGranted = await locationController.requestPermission();
+      if (_permissionGranted == PermissionStatus.denied) {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          duration: Duration(seconds: 5),
+          backgroundColor: Colors.red,
+          content: Text('Location permissions are denied'),
+        ));
+      }
+    }
+
+    locationController.onLocationChanged.listen((event) {
+      if (event.latitude != null && event.longitude != null) {
+        setState(() {
+          currentPosition = event;
+
+        });
+      }
+    });
+
   }
 
   @override
-  void initState() {
-
+  initState(){
+    getLocationUpdates();
     super.initState();
   }
+
+  @override
+  void setState(fn) {
+    if(mounted) {
+      super.setState(fn);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,44 +111,34 @@ class _VendorScreenState extends State<VendorScreen> {
           print("#########################################################3\n$val");
         },
         onMapCreated: (GoogleMapController controller) async {
-          _controller.complete(controller);
-          newGoogleMapController = controller;
-          PermissionStatus _permissionGranted;
-          serviceEnabled = await locationController.serviceEnabled();
-          if (!serviceEnabled) {
-            ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-              duration: Duration(seconds: 5),
-              backgroundColor: Colors.red,
-              content: Text('Location services are disabled.'),
-            ));
-            serviceEnabled = await locationController.requestService();
-          }
-          _permissionGranted = await locationController.hasPermission();
-          if (_permissionGranted == PermissionStatus.denied) {
-            _permissionGranted = await locationController.requestPermission();
-            if (_permissionGranted == PermissionStatus.denied) {
-              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
-                duration: Duration(seconds: 5),
-                backgroundColor: Colors.red,
-                content: Text('Location permissions are denied'),
-              ));
-            }
-          }
-          setState(() {
-            _locatePosition();
-          });
-          locationController.onLocationChanged.listen((event) {
-            if(event.latitude != null && event.longitude != null){
-
-            }
-          });
-
+          newGoogleMapController =  controller;
+          _controller.complete(newGoogleMapController);
+          CameraPosition cam = CameraPosition(target: LatLng(currentPosition?.latitude??0, currentPosition?.longitude??0), zoom: 20);
+          newGoogleMapController!.animateCamera(CameraUpdate.newCameraPosition(cam));
         },
         markers: {
-          Marker(markerId: MarkerId('1'), icon: BitmapDescriptor.defaultMarkerWithHue(100), position: LatLng(30.025603486746856, 31.438122242689136)),
-          Marker(markerId: MarkerId('2'), icon: BitmapDescriptor.defaultMarkerWithHue(100), position: LatLng(30.02723863572498, 31.44571825861931)),
+          Marker(
+            onTap: () {
+
+            },
+            markerId: MarkerId('1'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(100),
+            position: LatLng(30.025603486746856, 31.438122242689136),
+          ),
+          Marker(
+            markerId: MarkerId('2'),
+            icon: BitmapDescriptor.defaultMarkerWithHue(100),
+            position: LatLng(30.02723863572498, 31.44571825861931),
+          ),
         },
       ),
     );
+  }
+  @override
+  void dispose() {
+    newGoogleMapController?.dispose();
+
+    // TODO: implement dispose
+    super.dispose();
   }
 }
